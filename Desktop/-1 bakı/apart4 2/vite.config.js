f354535c0cab920b83601daba58d848e87a58a09
murat kavum <muratkavum@gmail.com>
2025-12-18 21:24:13 +0300

    fix: Add root to vite.config.js for Render build

--- Desktop/-1 bakı/apart4 2/vite.config.js
+++ Desktop/-1 bakı/apart4 2/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: process.cwd(),
   plugins: [react()],
   publicDir: 'public',
   server: {
