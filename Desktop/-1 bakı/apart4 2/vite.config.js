59b41acf1d2e44e40778a59a529e53134adfd20c
murat kavum <muratkavum@gmail.com>
2025-12-18 21:27:57 +0300

    fix: Use __dirname for root in vite.config.js

--- Desktop/-1 bakı/apart4 2/vite.config.js
+++ Desktop/-1 bakı/apart4 2/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: process.cwd(),
+  root: resolve(__dirname),
   plugins: [react()],
   publicDir: 'public',
   server: {
