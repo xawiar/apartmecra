6a92954c28875eb4be461d915ff539036b53cc4f
murat kavum <muratkavum@gmail.com>
2025-12-18 21:19:14 +0300

    fix: Set root to . in vite.config.js

--- Desktop/-1 bakı/apart4 2/vite.config.js
+++ Desktop/-1 bakı/apart4 2/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: process.cwd(),
+  root: '.',
   plugins: [react()],
   publicDir: 'public',
   server: {
