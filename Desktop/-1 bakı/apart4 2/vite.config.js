f89a06557c7887683eaa1c0735e5074eaeb696c9
murat kavum <muratkavum@gmail.com>
2025-12-19 17:38:39 +0300

    fix: Force correct directory in build command and vite config

--- Desktop/-1 bakı/apart4 2/vite.config.js
+++ Desktop/-1 bakı/apart4 2/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: resolve(__dirname),
   plugins: [react()],
   publicDir: 'public',
   server: {
